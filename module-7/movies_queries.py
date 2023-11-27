1a168acac863f2f3bd14db3d0797c3e9071134ab
nanirdz <a.evangelinardz@gmail.com>
2023-11-26 18:01:59 -0800

    Adding Module 7.2 Assignment

---
+++ module-7/movies_queries.py
@@ -0,0 +1,30 @@
+import movies
+from movies import errorcode
+
+config = {
+    "user": "movies_user",
+    "password": "popcorn",
+    "host": "127.0.0.1",
+    "database": "movies",
+    "raise_on_warnings": True
+}
+
+try:
+    db = movies.connect(**config)
+
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    input("n\n Press any key to continue...")
+
+except movies.connector.Error as err:
+    if err.errno ==errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username or password are invalid")
+    
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+
+    else:
+        print(err)
+    
+finally:
+    db.close()
